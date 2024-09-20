4b68201138b6367ba368716be5ecacc79f0a8b32
Pablo M. Cordero <corderopablomartin06@gmail.com>
2024-09-20 01:37:36 +0000

    Deploying project

--- deploy-to-github.js
+++ deploy-to-github.js
@@ -5,13 +5,22 @@ try {
   console.log("Building project...");
   execSync("npm run build", { stdio: "inherit" });
 
-  // Agregar, hacer commit y push a GitHub
+  // Agregar los cambios si los hay
   console.log("Deploying to GitHub...");
   execSync("git add .", { stdio: "inherit" });
-  execSync('git commit -m "Deploying project"', { stdio: "inherit" });
-  execSync("git push origin main", { stdio: "inherit" });
 
-  console.log("Deployment successful!");
+  // Verificar si hay cambios
+  const changes = execSync("git status --porcelain").toString().trim();
+
+  if (changes) {
+    // Si hay cambios, hacer commit y push
+    execSync('git commit -m "Deploying project"', { stdio: "inherit" });
+    execSync("git push origin main", { stdio: "inherit" });
+    console.log("Deployment successful!");
+  } else {
+    console.log("No changes to deploy.");
+  }
 } catch (error) {
   console.error(`Error during deployment: ${error.message}`);
 }
+
